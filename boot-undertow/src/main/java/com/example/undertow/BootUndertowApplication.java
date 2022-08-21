306389ddee7d32a5d9344992449fa1cf6e2e84f7
leellun <leellun@sina.cn>
2022-08-21 17:01:40 +0800

    --引入undertow--

---
+++ boot-undertow/src/main/java/com/example/undertow/BootUndertowApplication.java
@@ -0,0 +1,13 @@
+package com.example.undertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUndertowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUndertowApplication.class, args);
+    }
+
+}
---
+++ boot-undertow/src/test/java/com/example/undertow/BootUndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.undertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootUndertowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
