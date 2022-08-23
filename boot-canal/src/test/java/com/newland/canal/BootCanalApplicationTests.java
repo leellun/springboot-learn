9208f6c2b4975d0d6d3abc2350ff23d147edd0ce
leellun <leellun@sina.cn>
2022-08-24 00:58:29 +0800

    canal

---
+++ boot-canal/src/test/java/com/newland/canal/BootCanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
